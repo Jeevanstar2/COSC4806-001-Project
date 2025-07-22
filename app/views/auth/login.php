14e727a53a427ec7a04c10f569c716f7abd466f2
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-07-22 20:30:07 +0000

    added a login page and made minor changes

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,29 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Register</title>
+    <title>Login</title>
     <link rel="stylesheet" href="/public/css/styles.css">
 </head>
 <body>
-    <h2>Register</h2>
+    <h2>Login</h2>
 
     <?php if (!empty($error)): ?>
         <p style="color: red;"><?= htmlspecialchars($error) ?></p>
     <?php endif; ?>
-    
-    <form method="POST" action="index.php?action=register">
+
+    <form method="POST" action="index.php?action=login">
         <label>Username:</label><br>
         <input type="text" name="username" required><br><br>
 
         <label>Password:</label><br>
         <input type="password" name="password" required><br><br>
 
-        <label>Confirm Password:</label><br>
-        <input type="password" name="confirm" required><br><br>
-
-        <button type="submit">Create Account</button>
+        <button type="submit">Login</button>
     </form>
 
-    <p>Already have an account? <a href="index.php?action=loginForm">Login here</a></p>
+    <p>Don't have an account? <a href="index.php?action=registerForm">Register here</a></p>
+    <p>Or <a href="index.php">continue as guest</a></p>
 </body>
 </html>
