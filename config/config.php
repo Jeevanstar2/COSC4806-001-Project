9f0b11232f32d8057f1329013bb03a4ed3695550
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-07-22 19:00:26 +0000

    starting up the project by accessing the db first

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'host' => 'c0tme.h.filess.io',
+    'dbname' => 'COSC4806001JS2_figurewhom',
+    'username' => 'COSC4806001JS2_figurewhom',
+    'password' => $_ENV['PASS'],
+];
